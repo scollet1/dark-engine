6e31297b5918300bece0e791ff19fccc40d37c42
Samuel Collet <samuelpcollet@gmail.com>
2019-08-25 21:46:39 -0500

    oof

--- engine/config.sh
+++
@@ -1,7 +0,0 @@
-# install list
-# - boost++
-# - Vulkan SDK
-
-git submodule add https://github.com/glfw/glfw.git
-sudo apt-get install libboost-all-dev
-wget -c https://vulkan.lunarg.com/sdk/home#sdk/downloadConfirm/1.1.92.1/linux/vulkansdk-linux-x86_64-1.1.92.1.tar.gz -O - | tar -xz
